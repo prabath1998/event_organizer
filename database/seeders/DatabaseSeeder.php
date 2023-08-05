4bda7e50505b1f12f574c3a976271f421d0cec30
prabath1998 <prabhathudayanga1998@gmail.com>
2023-08-05 22:16:40 +0530

    create migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\City;
+use App\Models\Country;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +19,23 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        Country::create(['name' => 'United Kingdom']);
+        Country::create(['name' => 'Sri Lanka']);
+
+        City::create(['country_id' => 1, 'name' => 'London']);
+        City::create(['country_id' => 1, 'name' => 'Manchester']);
+        City::create(['country_id' => 1, 'name' => 'Liverpool']);
+        City::create(['country_id' => 2, 'name' => 'Galle']);
+        City::create(['country_id' => 2, 'name' => 'Colombo']);
+        City::create(['country_id' => 2, 'name' => 'Kandy']);
+
+        Tag::create(['name' => 'Laravel', 'slug' => 'laravel']);
+        Tag::create(['name' => 'Vue Js', 'slug' => 'vue-js']);
+        Tag::create(['name' => 'LiveWire', 'slug' => 'livewire']);
     }
 }
